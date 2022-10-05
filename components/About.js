7dacedaa2582ed599918859d2b6a93d551c80270
Helder Fonseca <helder2391@gmail.com>
2022-10-05 21:41:45 -0100

    change responsive class

--- components/About.js
+++ components/About.js
@@ -2,17 +2,16 @@ export default function About() {
     return(
         <section className="flex flex-col justify-center items-center mt-40 mx-auto gap-16 md:w-3/4 w-full">
             <h3 className="text-3xl font-bold text-center md:w-1/2 px-8">
-                O que o email marketing faz pela sua empresa?</h3>
+                What does email marketing do for your business?</h3>
 
             <div className="text-center md:w-1/2 w-full px-8">
-                <h4 className="font-bold text-[1.3rem]">Objetivos Gerais</h4>
-                <p>Informar; Atrair; Envolver; Guiar; Estimular; Converter.</p>
+                <h4 className="font-bold text-[1.3rem]">General Objectives</h4>
+                <p>Informs; Attracts; Involves; Guides; Encourage; and Converts</p>
             </div>
 
             <div className="text-center md:w-1/2 w-full px-8">
-                <h4 className="font-bold text-[1.3rem]">O que oferecemos?</h4>
-                <p className="mx-auto">Um banco de dados exclusivo; Um serviço dedicado que gera relatórios;
-                    Um produtor de conteúdo; Um designer.
+                <h4 className="font-bold text-[1.3rem]">What do we offer?</h4>
+                <p className="mx-auto">A unique database; A dedicated service that generates reports: A content producer; A designer.
                 </p>
             </div>
         </section>
--- components/Hero.js
+++ components/Hero.js
@@ -6,24 +6,23 @@ export default function Hero() {
 
     return(
         <div className="mx-auto max-w-6xl">
-            <div className="flex md:flex-row flex-col-reverse">
-                <div className="md:pb-[32rem] md:w-1/2 w-full">
+            <div className="flex md:flex-row flex-col-reverse gap-16">
+                <div className="md:w-1/2 w-full">
                     <Image src={heroImage} width={1090} height={1000}/>
                 </div>
-                <div className="mt-[4rem] md:pr-8 md:ml-16 md:w-1/2 w-full pb-8">
+                <div className="mt-[4rem] md:w-1/2 w-full">
                     <h1 className="text-5xl font-bold bg-[#FBED04] md:inline-block p-2 rounded-md">E-mail Marketing</h1>
-                    <h2 className="text-4xl md:text-left italic font-thin pt-3">Ganhe descontos e faça a aposta certeira</h2>
+                    <h2 className="text-4xl md:text-left italic font-thin pt-3">Get discounts and make the right bet!</h2>
                     <div className="pt-8">
                         <p className="leading-relaxed text-justify">
-                            O marketing por email é o uso do email para promover
-                            produtos ou serviços. Mas não se limita apenas a isso.
-                            Você pode usar o marketing por e-mail para devenvolver
-                            relações com os clientes atuais e / ou para chegar a
-                            clientes em potencial. O marketing por e-mail permite que
-                            as empresas mantenham seus clientes informados e 
-                            personalizem as suas mensagens de marketing. Veja nisso
-                            como uma das formas de marketing digital mais
-                            económicas e ricas em conversões da atualidade.
+                            Email marketing is the use of emails to promote 
+                            products or services. But it's not just limited to that. 
+                            You can use email marketing to develop relationships with 
+                            current customers or reach out to potential clients. 
+                            This service allows companies to keep their customers 
+                            informed and personalize their marketing messages. 
+                            Look at it as one of the most cost-effective and conversion-rich
+                             forms of digital marketing today.
                         </p>
                     </div>
                 </div>

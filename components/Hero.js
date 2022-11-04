3e0db330597b60ab8e5b1da75e5ddfe4a97c87c0
Helder Fonseca <helder2391@gmail.com>
2022-11-04 09:38:52 -0100

    change to black friday campaign

--- components/Hero.js
+++ components/Hero.js
@@ -8,11 +8,11 @@ export default function Hero() {
         <div className="mx-auto max-w-6xl">
             <div className="flex md:flex-row flex-col-reverse gap-16 md:mt-20">
                 <div className="md:w-1/2 w-full">
-                    <Image src={heroImage} width={1090} height={1000} alt="Marketing woman"/>
+                    <Image src={heroImage} width={1090} height={1000} alt="Black Friday"/>
                 </div>
                 <div className="mt-[4rem] md:w-1/2 w-full">
                     <h1 className="text-5xl font-bold bg-[#FBED04] md:inline-block p-2 rounded-md">E-mail Marketing</h1>
-                    <h2 className="text-4xl md:text-left italic font-thin pt-3">Get discounts and make the right bet!</h2>
+                    <h2 className="text-4xl md:text-left italic font-light pt-3">Enjoy <span className="font-bold">30% OFF</span> until the end of the year!</h2>
                     <div className="pt-8">
                         <p className="leading-relaxed text-justify">
                             Email marketing is the use of emails to promote 
